efede417be5bf16f8ed50ded371675cad217cc72
Salvador Guzman <guzmansalv@gmail.com>
2018-05-17 00:52:06 -0700

    removed redis

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import bs4
 import requests
 import os
 import brotli
-import redis
+import git
 
 limit = 1290
 redis_key = "ebooks"
-client = redis.StrictRedis()
-cache = client.hgetall(redis_key)
+repo_name = './ScrapeContent'
+dir_name = repo_name + '/txt/'
+repo = git.Repo(repo_name)
 
 
 def remove_prefix(text, prefix):
@@ -16,27 +17,19 @@ def remove_prefix(text, prefix):
     return text
 
 
-def create_dir(dir_name):
-    if not os.path.exists(dir_name):
-        print('Creating dir %s' % dir_name)
-        os.mkdir(dir_name)
+def create_dir(dir_name_dir):
+    if not os.path.exists(dir_name_dir):
+        print('Creating dir %s' % dir_name_dir)
+        os.mkdir(dir_name_dir)
 
 
-def get_redis(url):
-    if cache.get(url) is None:
-        html = requests.get(url).text
-        bro = brotli.compress(html.encode(), brotli.MODE_TEXT)
-        client.hset(redis_key, url, bro)
-        print(f'{url} -> {len(bro)}')
-    else:
-        html = brotli.decompress(cache[url])
-
-    return html
+def get(url):
+    return requests.get(url).text
 
 
 def get_links(i):
     url = f"http://23.95.221.108/page/{i}"
-    html = get_redis(url)
+    html = get(url)
 
     soup = bs4.BeautifulSoup(html, 'html.parser')
     arts = soup.findAll('article')
@@ -47,7 +40,7 @@ def get_links(i):
 
 def get_book(path):
     url = f"http://23.95.221.108/{path}"
-    return get_redis(url)
+    return get(url)
 
 
 def write(file_name, content):
@@ -58,6 +51,22 @@ def write(file_name, content):
 
 
 def main():
-    repo = git.Repo()
+    create_dir(dir_name)
+
+    for i in range(1, limit + 1):
+        for path in get_links(i):
+            file = f'./txt/{path}.txt.brotli'
+            if os.path.exists(file):
+                return
+            else:
+                print(file)
+
+                html = get_book(path)
+                bro = brotli.compress(html.encode(), brotli.MODE_TEXT)
+                write(repo_name + '/' + file, bro)
+
+                repo.index.add([file])
+                repo.index.commit(f'Added {file}')
+
 
 main()
